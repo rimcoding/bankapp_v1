8de1875bf182ebae998a75114863ef433a60a452
yog4130 <yog4130@gamil.com>
2023-04-14 14:22:45 +0900

    데이터 모델링 처리

---
+++ src/main/java/com/tenco/bank/repository/model/Account.java
@@ -0,0 +1,31 @@
+package com.tenco.bank.repository.model;
+
+import java.sql.Timestamp;
+
+import lombok.Data;
+/*
+ * 모델 클래스 (Value Object 역할만 하는것은 아니다)
+ * */
+@Data
+public class Account {
+	
+	private Integer id;
+	private String number;
+	private String password;
+	private Long balance;
+	private Integer userId;
+	private Timestamp createdAt;
+	
+	public void withdraw(Long amount) {
+		this.balance -= amount;
+	}
+	public void deposit(Long amount) {
+		this.balance += amount;
+	}
+	
+	// 생각해볼수 있는 메서드들
+	
+	// 패스워드 체크
+	// 잔액 여부 확인 (출금시)
+	// 계좌 소유자 확인
+}
